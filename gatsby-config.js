2d354699d5807391844a5e2e9d15a43383afbf60
Hatem <hatem@bentoandco.com>
2020-03-02 11:51:03 +0900

    inti gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hatem - web developer and freelancer`,
+    description: `Hatem, web developer and freelancer making webapps and websites for you.`,
+    author: `Hatem Houssein`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
