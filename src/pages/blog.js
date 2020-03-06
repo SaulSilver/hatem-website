0408290ac7483a214763f7d28577f47efaeaec8a
Hatem <hatem@bentoandco.com>
2020-03-06 11:17:38 +0900

    Use static query hook instead of component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,43 +1,44 @@
 import React from 'react';
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Fade from 'react-reveal/Fade';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const PostsQuery = graphql`
-  query PostsQuery {
-    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/posts/" } }) {
-      edges {
-        node {
-          id
-          frontmatter {
-            description
-            title
+const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query PostsQuery {
+      allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/posts/" } }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              description
+              title
+            }
           }
         }
       }
     }
-  }
-`;
-
-const renderPage = data => (
-  <Layout>
-    <SEO title="Blog" />
-    <div className="blog-content">
-      <h1>
-        <Fade bottom cascade>
-          Blog
-        </Fade>
-      </h1>
-      <div className="posts-container">
-        {data.allMarkdownRemark.edges.map((post, i) => (
-          <div className="post" key={i}>
-            {post.node.frontmatter.title}
-          </div>
-        ))}
+  `);
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <div className="blog-content">
+        <h1>
+          <Fade bottom cascade>
+            Blog
+          </Fade>
+        </h1>
+        <div className="posts-container">
+          {data.allMarkdownRemark.edges.map((post, i) => (
+            <div className="post" key={i}>
+              {post.node.frontmatter.title}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
-export default () => <StaticQuery query={PostsQuery} render={renderPage} />;
+export default Blog;
