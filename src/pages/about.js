9cd9bf9563b031536161058285684be0637b9bdd
Hatem <hatem@bentoandco.com>
2020-03-06 16:26:56 +0900

    About page HTML structure (WIP)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import Layout from '../components/layout';
+import Image from '../components/image';
 import SEO from '../components/seo';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const aboutText =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-
-const AboutPage = () => (
-  <Layout>
-    <SEO title="About" />
-    <div className="about-content">
-      <h1>
-        <Fade bottom cascade>
-          About
-        </Fade>
-      </h1>
-      <Fade bottom>
-        <p>{aboutText}</p>
-      </Fade>
-    </div>
-    {/* {data.ShowAboutImage ? (
-      <img src={data.aboutImage} alt="about iamge" />
-    ) : null} */}
-  </Layout>
-);
+const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allMarkdownRemark(filter: { frontmatter: { page: { eq: "about" } } }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              history
+              interests
+            }
+          }
+        }
+      }
+    }
+  `);
+  const pageContent = data.allMarkdownRemark.edges[0].node.frontmatter;
+  return (
+    <Layout>
+      <SEO title="About" />
+      <div className="about-content">
+        <h1>
+          <Fade bottom cascade>
+            About
+          </Fade>
+        </h1>
+        <Fade bottom>{<p>{pageContent.history}</p>}</Fade>
+        <Fade bottom>{<p>{pageContent.interests}</p>}</Fade>
+        {/* TODO: check Image component and show the right image */}
+        {/* <div>
+          <Image />
+        </div> */}
+      </div>
+    </Layout>
+  );
+};
 
 export default AboutPage;
