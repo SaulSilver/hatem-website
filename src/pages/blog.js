0c1ab476f51b5dc869f92d1134cc9531543d70d7
Hatem <hatem@bentoandco.com>
2020-03-06 17:53:45 +0900

    Blog post (WIP)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery, Link } from 'gatsby';
 import Fade from 'react-reveal/Fade';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Blog = () => {
-  const data = useStaticQuery(graphql`
-    query PostsQuery {
-      allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/posts/" } }) {
-        edges {
-          node {
-            id
-            frontmatter {
-              description
-              title
-            }
+const PostLink = (post, i) => {
+  const title = post.node.frontmatter.title;
+  return (
+    <Link to={`blog/${title}`} className="post" key={i}>
+      {title}
+    </Link>
+  );
+};
+
+const postsQuery = graphql`
+  query PostsQuery {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/posts/" } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
           }
         }
       }
     }
-  `);
+  }
+`;
+
+const Blog = () => {
+  const data = useStaticQuery(postsQuery);
   return (
     <Layout>
       <SEO title="Blog" />
@@ -30,11 +41,7 @@ const Blog = () => {
           </Fade>
         </h1>
         <div className="posts-container">
-          {data.allMarkdownRemark.edges.map((post, i) => (
-            <div className="post" key={i}>
-              {post.node.frontmatter.title}
-            </div>
-          ))}
+          {data.allMarkdownRemark.edges.map(PostLink)}
         </div>
       </div>
     </Layout>
---
+++ src/templates/post.js
@@ -0,0 +1,95 @@
+// import React from 'react';
+// import Fade from 'react-reveal/Fade';
+// import Layout from '../components/layout';
+// import SEO from '../components/seo';
+// import PageNav from '../components/page-nav';
+
+// const Post = ({ currentPost, previousPost, nextPost, ...props }) => (
+//   <Layout>
+//     <SEO title="Blog post" />
+//     <h1>
+//       <Fade bottom cascade>
+//         {currentPost.title}
+//       </Fade>
+//     </h1>
+//     <div>{currentPost.description}</div>
+//     <PageNav previous={previousPost} next={nextPost} />
+//   </Layout>
+// );
+
+// export default Post;
+
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { rhythm, scale } from '../utils/typography';
+
+const BlogPostTemplate = ({ data, pageContext, location }) => {
+  const post = data.markdownRemark;
+  const siteTitle = data.site.siteMetadata.title;
+  const { previous, next } = pageContext;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article>
+        <header>
+          <h1>{post.frontmatter.title}</h1>
+          <p>{post.frontmatter.date}</p>
+        </header>
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr />
+        <footer>
+          <Bio />
+        </footer>
+      </article>
+
+      <nav>
+        <ul>
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  );
+};
+
+export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+      }
+    }
+  }
+`;
