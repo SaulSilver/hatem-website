772baf73cb09d276c64e261ee9690767f1dfe406
Hatem <hatem@bentoandco.com>
2020-03-09 10:58:45 +0900

    Handle empty next and previous links

--- src/components/page-nav.js
+++ src/components/page-nav.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const PageNav = ({ previous, next }) => (
-  <nav>
-    <ul>
-      <li>
-        <Link to={previous.url}>{`< ${previous.title}`}</Link>
-      </li>
-      <li>
-        <Link to={next.url}>{`${next.title}>`}</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const PageNav = ({ previous, next }) =>
+  !previous && !next ? (
+    ''
+  ) : (
+    <nav>
+      <ul>
+        {!previous ? (
+          ''
+        ) : (
+          <li>
+            <Link to={previous.path}>{`< ${previous.title}`}</Link>
+          </li>
+        )}
+        {!next ? (
+          ''
+        ) : (
+          <li>
+            <Link to={next.path}>{`${next.title}>`}</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 
 export default PageNav;
