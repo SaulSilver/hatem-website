c37e90782b1e65066ec4383777cff8f4389eb3d7
Hatem <hatem@HatemnoMacBook-Pro.local>
2020-03-25 14:47:16 +0900

    Logo and header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,33 @@
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
 
-const Header = props => {
+const Header = () => {
+  const { homeImg } = useStaticQuery(graphql`
+    query {
+      homeImg: allImageSharp(
+        filter: { fixed: { originalName: { eq: "logo.png" } } }
+      ) {
+        nodes {
+          fixed(width: 32) {
+            ...GatsbyImageSharpFixed_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <header>
       <nav className="site-nav">
         <ul>
           <li>
             <Link to="/" activeClassName="active">
-              Home
+              <Img
+                alt="Hatem Houssein logo"
+                fluid={homeImg.nodes[0].fixed}
+                style={{ width: '42px', height: '42px' }}
+              />
             </Link>
           </li>
           <li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,19 @@ const HomePage = props => {
     <Layout pagePath={props.path} className="home">
       <SEO title="Home" />
       <h3 className="profession">Web developer and freelancer</h3>
-      <Fade delay={500}>
+      <Fade cascade delay={500}>
         <h1 className="name">Hatem.</h1>
       </Fade>
       <Fade delay={1200}>
         <h3 className="do">Making web apps and websites for</h3>
       </Fade>
-      <Fade delay={2000}>
+      <Fade cascade delay={2000}>
         <h2 className="you">YOU</h2>
       </Fade>
       {/* <Fade delay={2400}> */}
       <Link to="/contact" className="btn">
         Contact me
       </Link>
-
       {/* </Fade> */}
     </Layout>
   );
