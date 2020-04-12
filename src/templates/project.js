3b73e377fb72503a76bf123e7fe8acc1c210501f
Hatem <hatem@bentoandco.com>
2020-04-12 23:52:04 +0900

    Template date handling and image (WIP)

--- src/templates/project.js
+++ src/templates/project.js
@@ -3,15 +3,32 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import PageNav from '../components/page-nav';
+import Img from 'gatsby-image';
+import moment from 'moment';
+// TODO: move to component
+const DATE_FORMAT = 'DD MMMM YYYY';
+const formatDate = ({ from, to }) => {
+  const fromMoment = moment(from);
+  const toMoment = moment(to);
+  return `${fromMoment.format(DATE_FORMAT)} - ${
+    toMoment.isValid() ? toMoment.format(DATE_FORMAT) : 'Now'
+  }`;
+};
 
 const Project = ({ data, pageContext }) => {
   const { frontmatter, html } = data.markdownRemark;
+  const { title, from, to, projectImage } = frontmatter;
   const { next, previous } = pageContext;
   return (
     <Layout>
       <SEO title="Project" />
-      <h1>{frontmatter.title}</h1>
-      <h2>{frontmatter.date}</h2>
+      <h1>{title}</h1>
+      <small>{formatDate({ from, to })}</small>
+      <Img
+        className="project-image"
+        alt={`${title} image`}
+        fluid={projectImage.childImageSharp.fluid}
+      />
       <div
         className="blog-post-content"
         dangerouslySetInnerHTML={{ __html: html }}
@@ -26,8 +43,16 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        from
+        to
         title
+        projectImage {
+          childImageSharp {
+            fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
