deb66d3d8a3e5a7822abfcf1733b425879667158
Hatem <hatem@bentoandco.com>
2020-04-29 18:40:04 +0900

    Header css and html fixes

--- src/components/header.js
+++ src/components/header.js
@@ -1,58 +1,24 @@
-import { Link, useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import { Link } from 'gatsby';
 import React from 'react';
 
-const Header = () => {
-  const { homeImg } = useStaticQuery(graphql`
-    query {
-      homeImg: allImageSharp(
-        filter: { fixed: { originalName: { eq: "logo.png" } } }
-      ) {
-        nodes {
-          fixed(width: 32) {
-            ...GatsbyImageSharpFixed_withWebp_noBase64
-          }
-        }
-      }
-    }
-  `);
+const Header = () => (
+  <header>
+    <nav className="site-nav">
+      <Link to="/" className="home-link" activeClassName="active"></Link>
+      <Link to="/about" activeClassName="active">
+        About
+      </Link>
+      <Link to="/work" activeClassName="active">
+        Work
+      </Link>
+      <Link to="/blog" activeClassName="active">
+        Blog
+      </Link>
+      <Link to="/contact" activeClassName="active">
+        Contact
+      </Link>
+    </nav>
+  </header>
+);
 
-  return (
-    <header>
-      <nav className="site-nav">
-        <ul>
-          <li>
-            <Link to="/" activeClassName="active">
-              <Img
-                alt="Hatem Houssein logo"
-                fixed={homeImg.nodes[0].fixed}
-                style={{ width: '42px', height: '42px' }}
-              />
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" activeClassName="active">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/work" activeClassName="active">
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link to="/blog" activeClassName="active">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" activeClassName="active">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
 export default Header;
